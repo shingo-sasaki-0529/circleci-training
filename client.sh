53c045f4a9ef1931c08813db8ba55c641bbaa1bb
Shingo.sasaki <shingo.sasaki@studist.jp>
2020-01-20 00:23:22 +0900

    curlでパラメータを渡すように

--- client.sh
+++ client.sh
@@ -1,3 +1,7 @@
 curl -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d '{
-  "branch": "master"
+  "branch": "master",
+  "parameters": {
+    "runSpecialWorkflow": true,
+    "specialWorkflowText": "from client.sh"
+  }
 }' https://circleci.com/api/v2/project/github/shingo-sasaki-0529/circleci-training/pipeline
